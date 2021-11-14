ed5c3414e90877364bb36659c3dac7e41b8c4860
rptseng <tseng_r@hotmail.com>
2021-11-14 16:20:19 -0500

    added total_vote counter

--- PyPoll.py
+++ PyPoll.py
@@ -7,18 +7,23 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
-    txt_file.write("Counties in the Election\n------------------\nArapahoe\nDenver\nJefferson")
+#Initialize the total vote counter.
+total_votes = 0
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
-
-     # To do: perform analysis.
      file_reader = csv.reader(election_data)
-     #print the header row
+     
+     #Read the header row
      headers = next(file_reader)
-     print(headers)
+     
+     #print each row int he CSV file.
+     for row in file_reader:
+         #Add to the total vote count.
+         total_votes += 1
+print(total_votes)
+
+
 
 
     
